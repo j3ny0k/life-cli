5ef54f1324a5a463f36e61a6f1aee2d1b2f55aed
j3ny0k <jenyok1.krutoi@gmail.com>
2026-04-07 16:46:27 +0300

    first commit

---
+++ expenses.py
@@ -0,0 +1,169 @@
+import json
+
+
+def save_expenses(expenses):
+    with open("expenses.json", "w") as f:
+        json.dump(expenses, f)
+
+
+def load_expenses():
+    try:
+        with open("expenses.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+
+
+expenses = load_expenses()
+print(f"loaded {len(expenses)} expenses\n")
+
+commands = ("add", "show", "total", "by_category", "delete", "exit", "help")
+
+
+def input_num():
+    while True:
+        try:
+            num_str = input("\nexpense num: ")
+            if not num_str:
+                print("input is empty")
+                continue
+            num = int(num_str)
+        except ValueError:
+            print("only integers allowed")
+            continue
+
+        if not expenses:
+            print("no expenses")
+            return
+
+        if num < 1 or num > len(expenses):
+            print("invalid expense number")
+            continue
+
+        return num
+
+
+def add_expense():
+    while True:
+        name = input("\nname: ")
+        if not name:
+            print("empty input")
+            continue
+        break
+
+    while True:
+        amount_str = input("\namount: ")
+
+        if not amount_str:
+            print("input is empty")
+            continue
+
+        try:
+            amount = int(amount_str)
+            break
+        except ValueError:
+            print("only integers allowed")
+
+    while True:
+        category = input("\ncategory: ")
+        if not category:
+            print("input is empty")
+            continue
+        break
+
+    expenses.append({"name": name, "amount": amount, "category": category})
+
+    save_expenses(expenses)
+
+
+def show_expenses():
+    if not expenses:
+        print("no expenses")
+        return
+
+    num = 1
+
+    for expense in expenses:
+        name = expense["name"]
+        amount = expense["amount"]
+        category = expense["category"]
+
+        print(f"{num}. {name} – {amount} – {category}")
+        num += 1
+
+
+def total_expenses():
+    total = 0
+
+    for expense in expenses:
+        total += expense["amount"]
+
+    print(f"total: {total}")
+
+
+def by_category():
+    while True:
+        category_name = input("\nby_category: ")
+        if not category_name:
+            print("input is empty")
+            continue
+        break
+
+    total = 0
+    found = False
+
+    for expense in expenses:
+        if expense["category"] == category_name:
+            total += expense["amount"]
+            found = True
+
+    if not found:
+        print("category not found")
+    else:
+        print(f"{category_name}: {total}")
+
+
+def delete_expense():
+    num = input_num()
+
+    if num is None:
+        return
+
+    del expenses[num - 1]
+
+    save_expenses(expenses)
+
+    print("expense deleted")
+
+
+print('type "help" to show commands')
+
+while True:
+    command = input("\ncommand: ")
+    if not command:
+        print("input is empty")
+
+    elif command == "help":
+        print("allowed commands:", ", ".join(commands))
+
+    elif command not in commands:
+        print("invalid command:", command)
+        print("available:", ", ".join(commands))
+
+    elif command == "exit":
+        break
+
+    elif command == "add":
+        add_expense()
+
+    elif command == "show":
+        show_expenses()
+
+    elif command == "total":
+        total_expenses()
+
+    elif command == "by_category":
+        by_category()
+
+    elif command == "delete":
+        delete_expense()
---
+++ tasks.py
@@ -0,0 +1,131 @@
+import json
+
+
+def save_tasks(tasks):
+    with open("tasks.json", "w") as f:
+        json.dump(tasks, f)
+
+
+def load_tasks():
+    try:
+        with open("tasks.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+
+
+tasks = load_tasks()
+print(f"loaded {len(tasks)} tasks")
+
+commands = ("add", "show", "done", "delete", "exit", "help")
+
+
+def input_num():
+    while True:
+        try:
+            num_str = input("\ntask num: ")
+            if not num_str:
+                print("input is empty")
+                continue
+            num = int(num_str)
+        except ValueError:
+            print("only integers allowed")
+            continue
+
+        if not tasks:
+            print("no tasks")
+            return
+
+        if num < 1 or num > len(tasks):
+            print("invalid task number")
+            continue
+
+        return num
+
+
+def add_task():
+    while True:
+        title = input("\ntask: ")
+        if not title:
+            print("input is empty")
+            continue
+        break
+
+    tasks.append({"title": title, "done": False})
+
+    save_tasks(tasks)
+
+
+def show_tasks():
+    if not tasks:
+        print("no tasks")
+        return
+
+    num = 1
+    for task in tasks:
+        if task["done"]:
+            done = "[x]"
+        else:
+            done = "[ ]"
+        print(f"{num}. {done} {task['title']}")
+        num += 1
+
+
+def done_task():
+    num = input_num()
+
+    if num is None:
+        return
+
+    tasks[num - 1]["done"] = not tasks[num - 1]["done"]
+    save_tasks(tasks)
+
+    if tasks[num - 1]["done"]:
+        print("task marked as done")
+    else:
+        print("task marked as not done")
+
+
+def delete_task():
+    num = input_num()
+
+    if num is None:
+        return
+
+    del tasks[num - 1]
+
+    save_tasks(tasks)
+
+    print("task deleted")
+
+
+print('type "help" to show commands')
+
+while True:
+    command = input("\ncommand: ")
+
+    if not command:
+        print("input is empty")
+        continue
+
+    elif command == "help":
+        print("allowed commands:", ", ".join(commands))
+
+    elif command not in commands:
+        print("invalid command:", command)
+        print("available:", ", ".join(commands))
+
+    if command == "exit":
+        break
+
+    elif command == "add":
+        add_task()
+
+    elif command == "show":
+        show_tasks()
+
+    elif command == "done":
+        done_task()
+
+    elif command == "delete":
+        delete_task()
